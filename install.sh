166e07fca6551a44d188ea84b3f698a908cfde17
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-11-25 08:16:16 -0500

    Configure Alacritty

--- install.sh
+++ install.sh
@@ -11,7 +11,9 @@ ln -s ~/.config/dotfiles/tmux.conf .tmux.conf &
 # Oh-My-Zsh Auto suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &
 ln -s ~/.config/dotfiles/oh-my-zsh/autosuggest.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom} &
-sed -i 's/plugins=(/plugins=(zsh-autosuggestions /g' .zshrc
+sed -i 's/plugins=(/plugins=(zsh-autosuggestions /g' .zshrc &
+
+ln -s ~/.config/dotfiles/alacritty .config/ &
 
 wait
 echo "Installation done"
