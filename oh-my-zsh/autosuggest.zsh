f21a5e51688e06f67bca8b6c80ee9edfb3323a5c
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2020-12-22 07:40:01 -0500

    Oh-my-zsh: Consolidate custom commands in a single file.

--- oh-my-zsh/autosuggest.zsh
+++
@@ -1,2 +0,0 @@
-bindkey '^ ' autosuggest-accept
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#536878"
---
+++ oh-my-zsh/custom.zsh
@@ -0,0 +1,10 @@
+# vi key bindings
+bindkey -v
+
+# autosuggest
+bindkey '^ ' autosuggest-accept
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#536878"
+
+# fzf
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/completion.zsh
