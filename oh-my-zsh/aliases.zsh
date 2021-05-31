1de9e3967eba89be11ec17167e107e25e9ff40ce
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2021-05-31 09:58:41 -0400

    Update zsh aliases

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -1,15 +1,23 @@
 # Docker
-#alias dcu="dcup"
-#alias dcd="dcdn"
-#alias dcr="dcr --rm"
-#alias dcs="dcstop"
-#alias dclean="docker rmi -f $(docker images -q -f "dangling=true")"
-#alias ddestroy="docker rmi $(docker images -q | xargs)"
+alias doc="docker"
+alias di="docker images"
+alias dcls="docker container ls"
+alias dclean="docker rmi -f $(docker images -q -f "dangling=true")"
+alias ddestroy="docker rmi $(docker images -q | xargs)"
+alias dstat="docker stats --format 'table {{.Name}}\t{{.CPUPerc}}\t{{.MemPerc}}\t{{.NetIO}}'"
+
+# Docker Compose
+alias dc="docker-compose"
+alias dcu="docker-compose up"
+alias dcd="docker-compose down"
+alias dcr="docker-compose run --rm"
+alias dcs="docker-compose stop"
 
 # Git
-alias grm="git rebase master"
-
+alias grm="git rebase main"
 
 alias v=vim
 alias c=clear
 alias chmox="chmod u+x"
+
+alias -g G='| grep'
