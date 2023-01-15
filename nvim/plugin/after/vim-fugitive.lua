43152e2088f89bf7ea59f0f0e4c9a63b7a768377
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2023-01-15 14:23:53 -0500

    nvim: Add a fugitive mapping for git push -u.

--- nvim/plugin/after/vim-fugitive.lua
+++ nvim/plugin/after/vim-fugitive.lua
@@ -1,6 +1,8 @@
+local cur_branch = vim.fn.system('git rev-parse --abbrev-ref HEAD | tr -d "\n"')
+
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git, opts)
 vim.keymap.set('n', '<leader>ggp', function() vim.cmd.Git('push') end, opts)
 vim.keymap.set('n', '<leader>gpf', function() vim.cmd.Git('push -f') end, opts)
 vim.keymap.set('n', '<leader>ggl', function() vim.cmd.Git('pull') end, opts)
 vim.keymap.set('n', '<leader>gcob', ':G checkout -b ', opts)
---vim.keymap.set('n', '<leader>gpu', ':G push -u origin <C-R>=GetCurrentBranch()<CR><CR>', opts)
+vim.keymap.set('n', '<leader>gpu', function() vim.cmd.Git('push -u origin ' .. cur_branch) end, opts)
