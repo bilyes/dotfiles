6ef293ec034566a8f8f76c6bba0c2a3db794a268
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2023-03-24 15:50:35 -0400

    Nvim: Add nvim-tree and which-key plugins.

--- nvim/plugin/after/treesitter.lua
+++ nvim/plugin/after/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "ruby", "typescript", "go", "yaml", "lua", "vim", "help" },
+  ensure_installed = { "ruby", "typescript", "go", "java", "yaml", "lua", "vim", "help", "terraform" },
 
   highlight = {
     enable = true,
