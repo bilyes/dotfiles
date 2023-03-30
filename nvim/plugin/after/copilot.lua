412d03e9751fd8493de5b6b31496aebc811e9275
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2023-03-30 10:47:41 -0400

    nvim: Add some copilot mappings.

--- nvim/plugin/after/copilot.lua
+++ nvim/plugin/after/copilot.lua
@@ -1,4 +1,7 @@
 vim.g.copilot_filetypes = { xml = false }
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set('i', '<C-a>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+local opts = { silent = true, noremap = true, expr = true }
+vim.keymap.set('i', '<C-a>', 'copilot#Accept("<CR>")', opts)
+vim.keymap.set('i', '<C-f>', 'copilot#Next()', opts)
+vim.keymap.set('i', '<C-s>', 'copilot#Previous()', opts)
--- nvim/plugin/after/lsp.lua
+++ nvim/plugin/after/lsp.lua
@@ -1,5 +1,5 @@
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 --vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
