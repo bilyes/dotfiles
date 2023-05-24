1fcde307d6f3338b9afe0c16b5e5d20546dc4f31
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2023-05-24 17:07:12 -0400

    Manage plugins using lazy.vim instead of packer.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,25 @@
 require("basic")
 
-require("plugins")
+-- Manage plugins using packer
+--require("plugins_packer")
 
-require("mason").setup()
-require("mason-lspconfig").setup()
+-- Manage plugins using lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Must be defined before loading plugins with lazy
+vim.g.mapleader = " "
+
+require('lazy').setup('plugins')
 
 require("mappings")
-
