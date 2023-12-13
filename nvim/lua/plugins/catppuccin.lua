6d00576ab95426fce66099332f85d2f3f91d81f6
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2023-12-13 16:06:30 -0500

    nvim: Cleanup catppuccin config. (#45)

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,11 @@
 return {
     'catppuccin/nvim',
     config = function()
-        vim.cmd.colorscheme('catppuccin-macchiato')
-        --vim.cmd.colorscheme('catppuccin')
-        --vim.cmd.colorscheme('catppuccin-frappe')
+        require("catppuccin").setup({
+            flavour = "macchiato", -- latte, frappe, macchiato, mocha
+            transparent_background = true,
+        })
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        vim.cmd.colorscheme('catppuccin')
     end
 }
