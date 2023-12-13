f9511443708b56e9a3874fbae6a842f8a6356eff
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2023-12-13 16:22:34 -0500

    nvim: Fix copilot config to prevent keycode replacement. (#46)

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
         vim.g.copilot_filetypes = { lua = true, java = true, go = true, yaml = true, typescript = true }
         vim.g.copilot_no_tab_map = true
 
-        local opts = { silent = true, noremap = true, expr = true }
+        local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
         vim.keymap.set('i', '<C-a>', 'copilot#Accept("<CR>")', opts)
         vim.keymap.set('i', '<C-f>', 'copilot#Next()', opts)
         vim.keymap.set('i', '<C-s>', 'copilot#Previous()', opts)
