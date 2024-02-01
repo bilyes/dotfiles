902c5be48d10cddc1093db5f0cb7a2f8b4f256e3
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2024-02-01 13:54:11 -0500

    nvim: fix cmp config. (#50)

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -25,7 +25,7 @@ return {
 
             -- Actions
             --map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-            --map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+            map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
             --map('n', '<leader>hS', gs.stage_buffer)
             --map('n', '<leader>hu', gs.undo_stage_hunk)
             --map('n', '<leader>hR', gs.reset_buffer)
