14059abbe6b96f31606ae791cc0e4d6b3470e3ae
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2025-06-19 16:22:03 -0400

    zsh: update fzf config. (#95)

--- oh-my-zsh/custom.zsh
+++ oh-my-zsh/custom.zsh
@@ -13,14 +13,7 @@ bindkey '^I ' autosuggest-accept
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#536878"
 
 # fzf
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --ignore .cache --ignore .npm -f -g "" --depth 10' # requires silversearch-ag
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --ignore .cache --ignore .npm --ignore .cargo -f -g "" --depth 10' # requires silversearch-ag
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_PREVIEW_CMD="cat {}"
-
-# install fzf by cloning the git repo and running: ./install --xdg --no-bash --no-fish
-#source $HOME/.config/fzf/fzf.zsh
-
-# OR install through apt and
-
-source /usr/share/doc/fzf/examples/key-bindings.zsh
-source /usr/share/doc/fzf/examples/completion.zsh
+source <(fzf --zsh)
