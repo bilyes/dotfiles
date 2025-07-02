54a7b122665e4c22af5f37711d57fb5b32698607
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2025-07-02 08:06:43 -0400

    fuzzel: update file opening script. (#102)

--- hypr/scripts/fuzzel-open-file.sh
+++ hypr/scripts/fuzzel-open-file.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Set the path to your documents directory
+DOCS=
+
 if [ -z $(pidof fuzzel) ]; then
-    xdg-open $(fd -e ods -e odt -e pdf . ~/documents/ | fuzzel --dmenu --counter)
+    xdg-open $(fd -e ods -e odt -e pdf . $DOCS | fuzzel --dmenu --counter --prompt "󰈞 ")
 else
     pkill fuzzel
 fi
