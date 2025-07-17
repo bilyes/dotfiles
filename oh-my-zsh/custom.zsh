3bc416fc5bbef1cdf0a4d5291ac6fabf7422bdb3
Ilyess Bachiri <ilyess.bachiri@protonmail.com>
2025-07-17 08:00:44 -0400

    zsh: use CTRL-v to paste instead of the default CTRL-SHIFT-v.

--- oh-my-zsh/custom.zsh
+++ oh-my-zsh/custom.zsh
@@ -1,6 +1,17 @@
 # vi key bindings
 bindkey -v
 
+# Paste with Ctrl-v
+function zle-clipboard-paste {
+  if ((REGION_ACTIVE)); then
+    zle kill-region
+  fi
+  LBUFFER+="$(wl-paste)"
+}
+zle -N zle-clipboard-paste
+
+bindkey '^V' zle-clipboard-paste
+
 # Edit command
 bindkey -M vicmd v edit-command-line
 
@@ -17,3 +28,5 @@ export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --ignore .cache --ignore .
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_PREVIEW_CMD="cat {}"
 source <(fzf --zsh)
+
+export EDITOR=nvim
