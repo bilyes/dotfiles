f29d9d1aa6b8930219b909563c4e80144b3b2670
Ilyess Bachiri <29066939+bilyes@users.noreply.github.com>
2025-12-07 08:42:38 -0500

    nvim: Update DAP UI design elements. (#119)

--- nvim/plugin/after/dap.lua
+++ nvim/plugin/after/dap.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg=0, fg='#993939', bg='#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg=0, fg='#61afef', bg='#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg=0, fg='#98c379', bg='#31353f' })
+
+vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+
 local dap = require('dap')
 
 local opts = { noremap=true, silent=true }
@@ -10,6 +20,8 @@ vim.keymap.set('n', '<leader>dsi', ':lua require("dap").step_into()<CR>', opts);
 vim.keymap.set('n', '<leader>dso', ':lua require("dap").step_out()<CR>', opts);
 
 local dapui = require('dapui')
+
+vim.keymap.set('n', '<leader>de', ':lua require("dapui").eval()<CR>', opts);
 
 dapui.setup({
     layouts = {
