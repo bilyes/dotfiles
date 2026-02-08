d85a60b8cd9f40d7d6c9490aaa9a79ac1ba7aa68
Ilyess Bachiri <29066939+bilyes@users.noreply.github.com>
2026-02-07 19:36:16 -0500

    nvim: add opencode plugin. Some other minor plugin config updates.

--- nvim/lua/plugins/daily-notes-index.lua
+++ nvim/lua/plugins/daily-notes-index.lua
@@ -3,5 +3,10 @@ return {
     opts = {
         daily_notes_folder = "~/notes/main/diary",
         index_filename = "diary.md",
-    }
+        index_title = "Diary Index",
+    },
+    keys = {
+        { "<leader>di", ":DailyNotesIndex open<CR>", desc = "Open daily notes index" }
+    },
+    lazy = false
 }
