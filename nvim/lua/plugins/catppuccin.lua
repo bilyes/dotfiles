84fbd49ce984566cb796e2ff633561f8c0428e1e
Ilyess Bachiri <29066939+bilyes@users.noreply.github.com>
2026-03-30 12:32:21 -0400

    nvim: improve the hover buffer look for better visibility. (#174)

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,5 +4,7 @@ return {
     config = function()
         -- latte, frappe, macchiato, mocha
         vim.cmd.colorscheme('catppuccin-macchiato')
+        -- Make float windows (e.g. LSP hover) more visible
+        vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#8aadf4" })
     end
 }
