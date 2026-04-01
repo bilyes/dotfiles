1e98027164aef9ce25ca0f9ac812794ee0545471
Ilyess Bachiri <29066939+bilyes@users.noreply.github.com>
2026-04-01 08:15:20 -0400

    nvim: Add diffview plugin.

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -24,15 +24,28 @@ return {
             end, { expr = true })
 
             -- Actions
-            --map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+            map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
             map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
             --map('n', '<leader>hS', gs.stage_buffer)
-            --map('n', '<leader>hu', gs.undo_stage_hunk)
+            map('n', '<leader>hu', gs.undo_stage_hunk)
             --map('n', '<leader>hR', gs.reset_buffer)
             map('n', '<leader>hp', gs.preview_hunk)
             map('n', '<leader>hb', function() gs.blame_line { full = true } end)
             map('n', '<leader>tb', gs.toggle_current_line_blame)
-            map('n', '<leader>hd', gs.diffthis)
+            map('n', '<leader>hd', function()
+                if vim.wo.diff then
+                    -- Close the diff view
+                    vim.cmd('diffoff!')
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        local buf = vim.api.nvim_win_get_buf(win)
+                        if vim.api.nvim_buf_get_name(buf):match('^gitsigns://') then
+                            vim.api.nvim_win_close(win, true)
+                        end
+                    end
+                else
+                    gs.diffthis()
+                end
+            end)
             map('n', '<leader>hD', function() gs.diffthis('~') end)
             map('n', '<leader>td', gs.toggle_deleted)
 
