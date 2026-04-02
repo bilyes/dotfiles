7b6bb55c6a8915cebcb8a24378ff4436b6784098
Ilyess Bachiri <29066939+bilyes@users.noreply.github.com>
2026-04-02 10:57:55 -0400

    nvim: add gitsigns mapping to stage buffer. (#185)

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -26,7 +26,7 @@ return {
             -- Actions
             map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
             map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-            --map('n', '<leader>hS', gs.stage_buffer)
+            map('n', '<leader>hS', gs.stage_buffer)
             map('n', '<leader>hu', gs.undo_stage_hunk)
             --map('n', '<leader>hR', gs.reset_buffer)
             map('n', '<leader>hp', gs.preview_hunk)
