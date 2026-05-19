3aa8c01829b4a185429422f6103f7610f9f0489a
Ilyess Bachiri <29066939+bilyes@users.noreply.github.com>
2026-05-19 16:15:39 -0400

    nvim: Improve Telescope find_files exclusion list.

--- oh-my-zsh/functions.zsh
+++ oh-my-zsh/functions.zsh
@@ -1,9 +1,9 @@
 function drmi() {
     # TODO Check if docker is installed. If not print an error msg
-   
+
     if [[ -z "$1" ]]; then
         echo "Please provide a string to use when matching image names. Usage: drmi my-img"
-        exit 1;
+        return 1
     fi
 
     docker rmi -f $(docker images --format "{{ .ID  }}" --filter "reference=$1*")
